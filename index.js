3064458458e7a466c90c951b4d712db5664c0e4e
Pranoy-B <pranoybarua0012@gmail.com>
2025-08-29 00:54:35 +0600

    call history section js script added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+
+const callHistory = []
+
 // heart toggle
 
 const hearts = document.querySelectorAll(".heart")
@@ -22,27 +26,58 @@ for(let i=0; i<callButtons.length; i++){
             document.getElementById("coins").innerText = newBalance
             const card = this.closest(".card");
             const serviceName = card.querySelector(".service-name").innerText;
-            const phoneNumber = card.querySelector(".service-number").innerText;
-            alert("Calling " + serviceName + " " +phoneNumber);
+            const serviceNumber = card.querySelector(".service-number").innerText;
+            alert("Calling " + serviceName + " " + serviceNumber);
         }
         else {
             alert("Not Enough Balance")
         }
+    
     })
 }
 
-// const callButtons = document.querySelectorAll(".call-btn")
-// for(let i=0; i<callButtons.length; i++){
-//     callButtons[i].addEventListener("click",function(){
-//         const coinBalance = parseInt(document.getElementById("coins").innerText)
-//         if(coinBalance >= 20){
-//             const callRate = 20
-//             let newBalance = coinBalance - callRate
-//             document.getElementById("coins").innerText = newBalance
-//             alert("Calling" + this.innerText)
-//         }
-//         else {
-//             alert("Not Enough Balance")
-//         }
-//     })
-// }
+// call history section
+
+const callBtns = document.querySelectorAll(".call-btn")
+for(let i=0; i<callBtns.length; i++){
+    callBtns[i].addEventListener("click",function(){
+        const card = this.closest(".card");
+        const serviceName = card.querySelector(".service-name").innerText;
+        const serviceNumber = card.querySelector(".service-number").innerText;
+        
+        const callData = {
+        name: serviceName,
+        number: serviceNumber,
+        time:  new Date().toLocaleTimeString()
+    };
+
+    callHistory.push(callData)
+    const callHistoryParent = document.getElementById("call-history-parent")
+    callHistoryParent.innerHTML = ""
+
+    for(const data of callHistory){
+        const div = document.createElement("div")
+        div.innerHTML = `
+
+                
+                <div class="bg-[#41ff6b1a] flex justify-between items-center mt-4 p-1"> 
+                    <div>
+                        <h2 class="font-semibold">${data.name}</h2>
+                        <p>${data.number}</p>
+                    </div>
+                    <div>${data.time}</div>
+                </div>
+                
+        `
+        callHistoryParent.appendChild(div)
+    }
+    
+    })
+}
+
+// call history clear button
+document.getElementById("clear-btn").addEventListener("click", function(){
+    const callHistoryClear = document.getElementById("call-history-parent")
+    callHistoryClear.innerHTML = ''
+    callHistoryClear.length = 0
+})
